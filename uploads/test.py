e0abd5c6f9808e917db959640baddb38a77aa712
Antony Morris <C18465716@mytudublin.ie>
2022-04-12 04:24:45 +0100

    removing unnecssary files

--- uploads/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import pyarrow.feather as feather
-
-df = pd.read_csv('car_evaluation.csv')
-df.to_feather('test.feather')
-df = pd.read_feather('C:\\Users\\Tony\\Desktop\\interimproject\\uploads\\test.feather')
-print(df.head)
