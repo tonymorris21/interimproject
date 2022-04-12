d7bb97a7172e8cb3ac2a7457f0a0535a7d9b29e2
Antony Morris <C18465716@mytudublin.ie>
2022-04-12 05:34:00 +0100

    fixes

--- project/main.py
+++ project/main.py
@@ -5,10 +5,6 @@ from flask_login import login_required, current_user
 main = Blueprint('main', __name__)
 from flask_login import current_user
 
-# https://github.com/shuhaowu/projecto/tree/master/projecto/apiv1/files
-# https://www.digitalocean.com/community/tutorials/how-to-structure-large-flask-applications
-# https://github.com/AmolMavuduru/AutoML-HackUTD19
-#https://stackoverflow.com/questions/62682674/how-to-get-dynamic-html-table-entries-in-a-form-to-flask
 @main.route('/')
 def index():
     if current_user.is_authenticated:
